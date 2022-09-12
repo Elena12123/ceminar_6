8938cbbfd614c21418f5db521b97bf4e5d3618ea
KEA <lena_konovalova@rambler.ru>
2022-09-12 18:55:05 +0300

    z41

---
+++ ceminar_6/задача_41/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.Write("Введите элементы (через пробел): ");
+string num = Console.ReadLine();
+var array = num.Split().Select(Int32.Parse).ToArray();
+int[] new_num = array;
+
+
+void Pologitelnie (int[] array)
+
+{
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0) count++;
+
+}
+Console.WriteLine($"Кодичуство чисел > 0: {count}");
+}
+Pologitelnie(new_num);
